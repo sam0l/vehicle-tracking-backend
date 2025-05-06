a3ab37e6b410ed4b89b09ee4554c07b3a151f4e4
sam0l <samsamolmol@gmail.com>
2025-05-06 16:43:18 +0800

    fixdsx more

--- app/database.py
+++ app/database.py
@@ -1,5 +1,13 @@
-from sqlalchemy import create_engine from sqlalchemy.orm import sessionmaker import os
+from sqlalchemy import create_engine 
+from sqlalchemy.orm import sessionmaker 
+import os
 
-DATABASE_URL = os.getenv("DATABASE_URL") engine = create_engine(DATABASE_URL) SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+DATABASE_URL = os.getenv("DATABASE_URL") 
+engine = create_engine(DATABASE_URL) 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def get_db(): db = SessionLocal() try: yield db finally: db.close()
+def get_db(): 
+    db = SessionLocal() 
+
+    try: yield db 
+    finally: db.close()
